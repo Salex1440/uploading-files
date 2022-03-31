eb2fff5dfdcbf9c3caf06d50e885bf25c0bf0b96
Salex <123@13.com>
2022-03-31 15:47:05 +0300

    Added file size restrictions. Minor fixes.

--- src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
+++ src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
@@ -4,7 +4,6 @@ import com.example.uploadingfiles.storage.StorageProperties;
 import com.example.uploadingfiles.storage.StorageService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
